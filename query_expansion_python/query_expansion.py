a99122575f7303a3c67c9e5935ba1ea423b4dd5b
ThomasM4nn <tommaso.green@studenti.unipd.it>
2021-04-09 23:13:01 +0200

    Second commit: implemented first test of query expansion with GPT-2 using greedy and BeamSearch approaches.

--- query_expansion_python/query_expansion.py
+++ query_expansion_python/query_expansion.py
@@ -1,5 +1,6 @@
 import os
-
+from transformers import GPT2Tokenizer, GPT2LMHeadModel, pipeline, set_seed, BeamSearchScorer
+import sys
 import xmltodict
 
 
@@ -15,10 +16,34 @@ def get_topic_list(filename):
 
 
 def main():
-    filename = "/home/yeagerists/seupd2021-yeager/topics-task-1.xml"
+    filename = str(sys.argv[1])
     topic_list = get_topic_list(filename)
-    for x in topic_list:
-        print(x)
+
+    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+    model = GPT2LMHeadModel.from_pretrained('gpt2')
+
+    # num_beams = 2
+    # beam_scorer = BeamSearchScorer(
+    #    batch_size=1,
+    #   max_length=model.config.max_length,
+    #  num_beams=num_beams,
+    #   device=model.device,
+    # )
+
+    num_query = 3
+    for query in topic_list[:num_query]:
+        inputs = tokenizer(query, return_tensors="pt")
+        greedy_output = model.generate(input_ids=inputs.input_ids, min_length=20, max_length=40,
+                                       return_dict_in_generate=True, output_scores=True)
+        beam_output = model.generate(input_ids=inputs.input_ids, num_beams=2, min_length=20, max_length=40,
+                                     num_return_sequences=1)
+
+        greedy_output = tokenizer.decode(greedy_output.sequences[0], skip_special_tokens=True)
+        beam_output = tokenizer.decode(beam_output[0], skip_special_tokens=True)
+        # beam_output = model.beam_search(inputs.input_ids, beam_scorer=beam_scorer, return_dict_in_generate=True, output_scores=True)
+        print(f"Original: {query} \n Greedy: {greedy_output} \n Beam Search: {beam_output}")
+
+    # inputs.input_ids {batch_size} --> {batch_size*num_beams}
 
 
 main()
