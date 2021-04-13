ec429e186a56bfddfb423054d1e4259f72444b32
ThomasM4nn <tommaso.green@studenti.unipd.it>
2021-04-13 19:33:01 +0200

    Committing first implementation of Argument Quality Model. Brief description of the files: - argquality_dataset.py contains the definition of the dataset (using PyTorchLightning Datamodules), which is read from the csv file named webis-argquality20-full.csv - model.py contains the Huggingface based implementation of the transformer-encoder + post-encoder layer (feedforward neural network for now) for Argument Quality - train.py trains the model of model.py using early stopping on the R^2 score on the validation set - demo.py provides a small demo, by printing arguments and related scores in non-decreasing order. It requires a checkpoint file not included in the commit, but available here shorturl.at/hzILZ

---
+++ argument_quality/model.py
@@ -0,0 +1,78 @@
+import pytorch_lightning as pl
+import torch
+import torch.nn as nn  # all neural network models
+from transformers import AutoModel, AutoTokenizer
+import argparse
+from torchmetrics import R2Score
+
+
+class ArgQualityModel(pl.LightningModule):
+
+    def __init__(self, model_name, learning_rate, weight_decay):
+        super().__init__()
+
+        self.learning_rate = learning_rate
+        self.weight_decay = weight_decay
+
+        # BERT encoder
+        self.encoder = AutoModel.from_pretrained(model_name)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.ffn = nn.Sequential(nn.Linear(768, 512), nn.ReLU(), nn.Linear(512, 256), nn.ReLU(), nn.Linear(256, 1))
+
+        self.loss_fn = nn.MSELoss()
+        self.train_r2score = R2Score()
+        self.val_r2score = R2Score()
+
+        for param in self.encoder.base_model.parameters():
+            param.requires_grad = False
+
+        self.save_hyperparameters()
+
+    def forward(self, batch):
+        batch_input = self.tokenizer(batch, padding=True, truncation=True, return_tensors='pt')
+        encoder_output = self.encoder(**batch_input, output_hidden_states=True)  # BERT pass
+        cls_emb = encoder_output.last_hidden_state[:, 0, :]
+        batch_scores = self.ffn(cls_emb)
+
+        textual_args = self.tokenizer.batch_decode(batch_input['input_ids'], skip_special_tokens=True)
+        arg_to_score = [(x, batch_scores[idx][0]) for idx, x in enumerate(textual_args)]
+        return arg_to_score
+
+    def training_step(self, batch, batch_idx):
+        batch_input, batch_targets = batch
+        encoder_output = self.encoder(**batch_input, output_hidden_states=True)  # BERT pass
+        cls_emb = encoder_output.last_hidden_state[:, 0, :]
+        batch_scores = self.ffn(cls_emb)
+
+        loss = self.loss_fn(batch_scores, batch_targets)
+        self.log('train_loss', loss, on_step=True, on_epoch=True)
+
+        if batch_targets.shape[0] >= 2:  # 2 samples at least needed for R2 computation
+            train_r2 = self.train_r2score(batch_scores, batch_targets)
+            self.log('train_r2', train_r2, on_step=True, on_epoch=True, prog_bar=True)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        batch_input, batch_targets = batch
+        encoder_output = self.encoder(**batch_input, output_hidden_states=True)  # BERT pass
+        cls_emb = encoder_output.last_hidden_state[:, 0, :]
+        batch_scores = self.ffn(cls_emb)
+
+        loss = self.loss_fn(batch_scores, batch_targets)
+        self.log('val_loss', loss, on_step=True, on_epoch=True, prog_bar=True)
+
+        if batch_targets.shape[0] >= 2:  # 2 samples at least needed for R2 computation
+            val_r2 = self.val_r2score(batch_scores, batch_targets)
+            self.log('val_r2', val_r2, on_step=False, on_epoch=True, prog_bar=True)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate, weight_decay=self.weight_decay)
+        return optimizer
+
+    @staticmethod
+    def add_specific_args(parent_parser):
+        parser = parent_parser.add_argument_group("ArgQualityModel")
+        parser.add_argument('-lr', '--learning_rate', type=float, default=1e-4)
+        parser.add_argument('-w', '--weight_decay', type=float, default=1e-5)
+        return parent_parser
