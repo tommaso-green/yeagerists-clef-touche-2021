fb4a8bfbe117bb05cf8c7f886ee48f5358c97972
Luca <lucamoro96@gmail.com>
2021-04-17 11:52:25 +0200

    Added cmd line args
    example args:
    -d /home/datasets/Args.me/data -o experiment/index-stop-nostem -t /home/datasets/Args.me/topics-task-1-only-titles.xml -r experiment

--- indexing/src/main/java/it/unipd/dei/yeagerists/Application.java
+++ indexing/src/main/java/it/unipd/dei/yeagerists/Application.java
@@ -3,12 +3,17 @@ package it.unipd.dei.yeagerists;
 import it.unipd.dei.yeagerists.index.DirectoryIndexer;
 import it.unipd.dei.yeagerists.search.Searcher;
 import lombok.extern.java.Log;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.core.LowerCaseFilterFactory;
 import org.apache.lucene.analysis.core.StopFilterFactory;
 import org.apache.lucene.analysis.custom.CustomAnalyzer;
+import org.apache.lucene.analysis.en.EnglishPossessiveFilterFactory;
+import org.apache.lucene.analysis.en.KStemFilterFactory;
 import org.apache.lucene.analysis.standard.StandardTokenizerFactory;
-import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.search.similarities.BM25Similarity;
 import org.apache.lucene.search.similarities.Similarity;
 
@@ -21,13 +26,22 @@ import java.util.logging.Logger;
 @Log
 public class Application {
 
-    public static void main(String... args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
         setLogLevel(Level.INFO);
 
+        CommandLine cli;
+        try {
+            cli = parseArgs(args);
+        } catch (org.apache.commons.cli.ParseException e) {
+            log.severe(String.format("Error parsing arguments: %s", e.getMessage()));
+            return;
+        }
+
+        final String docsPath = cli.getOptionValue("dataset");
+        final String indexPath = cli.getOptionValue("output");
+
         final int ramBuffer = 256;
-        final String docsPath = "/home/datasets/Args.me/data";
-        final String indexPath = "experiment/index-stop-nostem";
         final String extension = "json";
         final String charsetName = "ISO-8859-1";
 
@@ -35,6 +49,8 @@ public class Application {
                 .withTokenizer(StandardTokenizerFactory.class)
                 .addTokenFilter(LowerCaseFilterFactory.class)
                 .addTokenFilter(StopFilterFactory.class)
+                .addTokenFilter(EnglishPossessiveFilterFactory.class)
+                .addTokenFilter(KStemFilterFactory.class)
                 .build();
 
         final Similarity similarity = new BM25Similarity();
@@ -43,18 +59,32 @@ public class Application {
                 extension, charsetName);
         indexer.index();
 
-        final String topics = "/home/datasets/Args.me/topics-task-1-only-titles.xml";
-        final String runPath = "experiment";
-        final String runID = "yeagerists";
-        final int maxDocsRetrieved = 1000;
+        if (cli.hasOption("topics") && cli.hasOption("run")) {
+            final String topics = cli.getOptionValue("topics");
+            final String runPath = cli.getOptionValue("run");
 
-        final Searcher s = new Searcher(analyzer, similarity, indexPath, topics, runID, runPath, maxDocsRetrieved);
+            final String runID = "yeagerists";
+            final int maxDocsRetrieved = 1000;
 
-        try {
-            s.search();
-        } catch (ParseException e) {
-            log.severe(String.format("Unable to parse query: %s", e.getMessage()));
+            final Searcher s = new Searcher(analyzer, similarity, indexPath, topics, runID, runPath, maxDocsRetrieved);
+
+            try {
+                s.search();
+            } catch (org.apache.lucene.queryparser.classic.ParseException e) {
+                log.severe(String.format("Unable to parse query: %s", e.getMessage()));
+            }
         }
+    }
+
+    private static CommandLine parseArgs(String[] args) throws org.apache.commons.cli.ParseException {
+        Options options = new Options();
+        options.addRequiredOption("d", "dataset", true, "Path to Args.me dataset");
+        options.addRequiredOption("o", "output", true, "Path to directory where the index will be stored");
+        options.addOption("t", "topics", true, "Path to the topics file");
+        options.addOption("r", "run", true, "Path where the run file will be stored");
+
+        CommandLineParser parser = new DefaultParser();
+        return parser.parse(options, args);
     }
 
     private static void setLogLevel(Level level) {
