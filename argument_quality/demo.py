8304033d4ef330b0c81c1ddd640991cfcae874d1
ThomasM4nn <tommaso.green@studenti.unipd.it>
2021-04-21 18:29:55 +0200

    Tiny variations.

--- argument_quality/demo.py
+++ argument_quality/demo.py
@@ -11,8 +11,8 @@ def main():
     # Load dataset and extract sample_size arguments
     full_dataset = load_dataset('csv', data_files="../webis-argquality20-full.csv").shuffle(SEED)
     sample_size = 15
-    arg_batch = full_dataset['train'][:sample_size - 1]['Premise']  # List of strings of arguments
-    for x in arg_batch:
+    arg_batch = full_dataset['train'][:sample_size - 1]  # List of strings of arguments
+    for x in arg_batch['Premise']:
         print(x)
 
     # Load model from training checkpoint
@@ -21,12 +21,14 @@ def main():
 
     # Run model on the arguments: it
     # Returns a list of tuples [ .. (a,s(a)) ..] where a is the argument and s(a) is the score
-    output_list = arg_quality_model(arg_batch)
+    output_list = arg_quality_model(arg_batch['Premise'])
+    for idx, x in enumerate(output_list):
+        x.append(arg_batch['Combined Quality'][idx])
 
     output_list.sort(key=lambda x: x[1], reverse=True)  # Sort by decreasing scores
     for idx, element in enumerate(output_list):  # Print results
         print(f"\nArgument {idx + 1}:\n{element[0]}")
-        print(f"Score:\n{element[1]}")
+        print(f"Score: Predicted {element[1]} - Actual: {element[2]}")
 
 
 if __name__ == "__main__":
