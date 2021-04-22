f91e543753788921ad626576b2117dcb59ab075d
ThomasM4nn <tommaso.green@studenti.unipd.it>
2021-04-22 22:45:43 +0200

    Fixed argparser.

--- argument_quality/test.py
+++ argument_quality/test.py
@@ -9,18 +9,17 @@ from pytorch_lightning.loggers import WandbLogger
 
 def main():
     parser = argparse.ArgumentParser()
-    parser_2 = argparse.ArgumentParser()
     parser.add_argument('-s', '--seed', type=int, default=42)
     parser.add_argument('--gpus', type=int, default=1)
     parser.add_argument('--train_size', type=float, default=0.8)
     parser.add_argument('--val_size', type=float, default=0.1)
     parser.add_argument('--test_size', type=float, default=0.1)
     parser.add_argument('-m', '--model_name', type=str, default='bert-base-uncased')
-    parser_2.add_argument('--ckpt', type=str)
-    parser_2.add_argument('--id', type=str)
+    parser.add_argument('--ckpt', type=str)
+    parser.add_argument('--id', type=str)
     parser = ArgQualityDataset.add_specific_args(parser)
     args = parser.parse_args()
-    args_2 = parser_2.parse_args()
+
 
     pl.seed_everything(args.seed)
     split_dict = {'train': args.train_size, 'val': args.val_size, 'test': args.test_size}
@@ -29,10 +28,12 @@ def main():
 
     dm.prepare_data()
     dm.setup()
-    ckpt_file = "model_checkpoints/" + args_2.ckpt
+    ckpt_file = "model_checkpoints/" + args.ckpt
     arg_quality_model = ArgQualityModel.load_from_checkpoint(ckpt_file)
 
-    wandb.init(project="ArgumentQuality", name=args.model_name, resume=True, id=args_2.id)
+    wandb.init(project="ArgumentQuality", name=args.model_name, resume=True, id=args.id)
+    args.__delattr__("id")
+    args.__delattr__("ckpt")
     wandb.config.update(args)
     wandb_logger = WandbLogger()
     trainer = pl.Trainer(gpus=args.gpus, deterministic=True, logger=wandb_logger,
