9b757532e5e6c64fc994b04b6837b3d9bc5fcb2d
ThomasM4nn <tommaso.green@studenti.unipd.it>
2021-04-25 10:12:29 +0200

    Fixed bug with respath/querypath, added option --queryexp to activate query expansion. If alpha=0.0, then also reranking is skipped.

--- main.py
+++ main.py
@@ -30,12 +30,14 @@ def write_queries_to_file(path: str, queries: [str], ids: [str]):
             f.write("</topic>")
 
         f.write("</topics>")
+    f.close()
 
 
 def read_results(res_path: str):
     res = []
     with open(res_path) as f:
         res = json.load(f)
+    f.close()
     return res
 
 
@@ -52,17 +54,19 @@ def read_topics(topics_path):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--indexpath', type=str, default="data/index")
-    parser.add_argument('-q', '--querypath', type=str, default="data/res.txt")
-    parser.add_argument('-r', '--resultpath', type=str, default="data/_tmp_queries.xml")
+    parser.add_argument('-q', '--querypath', type=str, default="data/_tmp_queries.xml")
+    parser.add_argument('-r', '--resultpath', type=str, default="data/res.txt")
     parser.add_argument('-t', '--topicpath', type=str,
                         default="datasets/touche2021topics/topics-task-1-only-titles.xml")
     parser.add_argument('-c', '--ckpt', type=str, default="bert-base-uncased_best-epoch=04-val_r2=0.69.ckpt")
     parser.add_argument('-m', '--maxdocs', type=str, default="10")
+    parser.add_argument('-qe', '--queryexp', action='store_true')
     parser.add_argument('-a', '--alpha', type=float, default=0.3)
     parser.add_argument('-n', '--name', type=str, default="dev_run")
     args = parser.parse_args()
 
     topic_list = read_topics(args.topicpath)
+    print(f"Topic List size: {len(topic_list)}")
 
     arg_quality_model = ArgQualityModel.load_from_checkpoint("argument_quality/model_checkpoints/" + args.ckpt)
     arg_quality_model.eval()
@@ -72,18 +76,21 @@ def main():
 
     ids = [x[0] for x in topic_list]
     queries = [x[1] for x in topic_list]
-
-    start = datetime.now()
-    tot_queries = []
-    for q in queries:
-        print(f"Expanding query {q}")
-        ids, new_queries = expand_query(q)
-        tot_queries.append(new_queries)
-        write_queries_to_file(args.querypath, new_queries, ids)
-    end = datetime.now()
-    time_taken = end - start
-    print('Time taken for Query Expansion: ', time_taken)
-
+    if args.queryexp:
+        start = datetime.now()
+        print('-->Starting Query Expansion')
+        tot_queries = []  # just for debugging
+        for q in queries:
+            print(f"Expanding query {q}")
+            ids, new_queries = expand_query(q)
+            tot_queries.append(new_queries)
+            write_queries_to_file(args.querypath, new_queries, ids)
+        end = datetime.now()
+        time_taken = end - start
+        print('Time taken for Query Expansion: ', time_taken)
+    else:
+        print('\n--->No query expansion')
+        write_queries_to_file(args.querypath, queries, ids)
     java_args = f"--search --path {args.indexpath} --queries {args.querypath} --results {args.resultpath} --max {args.maxdocs}"
     os.system("java -jar indexing/target/indexing-1.0-SNAPSHOT-jar-with-dependencies.jar " + java_args)
 
@@ -97,26 +104,36 @@ def main():
         print("Doc stance: %s" % d["stance"])
         print("Doc score: %s" % d["score"])
 
-    arguments = [d["body"] for d in documents]
-    start = datetime.now()
-    arg_to_score = arg_quality_model(arguments)
-    end = datetime.now()
-    time_taken = end - start
-    print('Time for Query Expansion: ', time_taken)
+    if args.alpha > 0:
+        arguments = [d["body"] for d in documents]
+        args_with_score = []
+        start = datetime.now()
+        # todo check if there's any improvement by dividing arguments in small batches
+        for arg in arguments:
+            args_with_score += arg_quality_model(arg)
+        end = datetime.now()
+        time_taken = end - start
+        print('Time for Query Expansion: ', time_taken)
 
-    print("\n"+"*" * 5 + "RERANKED LIST" + "*" * 5)
-    for i, d in enumerate(documents):
-        d["total_score"] = (1 - args.alpha) * d["score"] + args.alpha * arg_to_score[i][1]
-    reranked_docs = sorted(documents, key=lambda d: (int(d["queryId"]), -d["total_score"]))
-    for i, d in enumerate(reranked_docs):
-        print("\n\n")
-        print("Query id: %s" % d["queryId"])
-        print("Doc id: %s" % d["id"])
-        print("Doc body: %s" % d["body"])
-        print("Doc stance: %s" % d["stance"])
-        print("Doc score: %s" % d["score"])
-        print("Doc quality: %s" % arg_to_score[i][1])
-        print("Doc total score: %s" % d["total_score"])
+        print("\n" + "*" * 5 + "RERANKED LIST" + "*" * 5)
+        for i, d in enumerate(documents):
+            d["total_score"] = (1 - args.alpha) * d["score"] + args.alpha * args_with_score[i][1]
+        reranked_docs = sorted(documents, key=lambda d: (int(d["queryId"]), -d["total_score"]))
+        for i, d in enumerate(reranked_docs):
+            print("\n\n")
+            print("Query id: %s" % d["queryId"])
+            print("Doc id: %s" % d["id"])
+            print("Doc body: %s" % d["body"])
+            print("Doc stance: %s" % d["stance"])
+            print("Doc score: %s" % d["score"])
+            print("Doc quality: %s" % args_with_score[i][1])
+            print("Doc total score: %s" % d["total_score"])
+
+    else:
+        print("\n--> No Argument Quality Reranking")
+        for i, d in enumerate(documents):
+            d["total_score"] = d["score"]
+        reranked_docs = sorted(documents, key=lambda d: (int(d["queryId"]), -d["total_score"]))
 
     rank = 0
     query_counter = reranked_docs[0]['queryId']
@@ -129,6 +146,8 @@ def main():
                 query_counter = d['queryId']
             f.write(f"{d['queryId']} QO {d['id']} {rank} {d['total_score']:.3f} {args.name}\n")
 
+    f.close()
+
 
 if __name__ == "__main__":
     start_m = datetime.now()
@@ -136,4 +155,3 @@ if __name__ == "__main__":
     end_m = datetime.now()
     time_taken = end_m - start_m
     print('Time: ', time_taken)
-
